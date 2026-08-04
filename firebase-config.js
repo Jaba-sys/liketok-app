18141e9d7dc8f1b2de317e0c7fbbfd55e220fce7
Jaba-sys <Sop3chit.xpol.viva0@gmail.com>
2026-08-04 19:15:04 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBdHDVqsShh9wf4BMy2_Hcwno5ie21dnBQ",
+  authDomain: "liketok-app.firebaseapp.com",
+  databaseURL: "https://liketok-app-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "liketok-app",
+  storageBucket: "liketok-app.firebasestorage.app",
+  messagingSenderId: "594119633746",
+  appId: "1:594119633746:web:4444aeda9474410630688b"
+};
